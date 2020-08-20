e8802500c73e61e23101b9cf0ebe889d5c1104db
arthurvids <69432390+arthurvids@users.noreply.github.com>
2020-08-20 17:23:15 +0100

    adding scrollvanish

---
+++ scrollvanish.js
@@ -0,0 +1,21 @@
+$(window).scroll(function(){
+    $(".logocontainer").css("opacity", 1 - $(window).scrollTop() / 150);
+  });
+
+/*win.scroll(function(){
+  scrollPosition = win.scrollTop();
+  scrollRatio = 1 - scrollPosition / 300;
+  $(".top").css("opacity", scrollRatio);
+*/
+
+
+
+
+
+/*$(window).scroll(function(){
+    var scrollVar = $(window).scrollTop();
+    $('.top').css("opacity", 1 - scrollVar/300);
+})*/
+
+
+
